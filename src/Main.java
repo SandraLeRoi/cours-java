df8d1ac8c81fada18705bac6ab8e40a2cda77d4e
SandraLeRoi <oawx72@gmail.com>
2021-03-15 10:19:28 +0100

    essai git

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int monNombre = 12; // stocké sur 32 bits
+        // nombre est stocké 2 puissance 31 byte en positif et en négatif
+        char monCaractère = 'b';
+        boolean monBool = true;
+        float monChiffre = 2.3f;
+        double unAutreNombreAVirgule = 2.3;
+        byte monOctet = 1; // stocké sur un octet
+        short petitInteger = 2; // stocké sur 16 bits
+        long grandInteger = 468; // stocké sur 64 bits
+
+        // /!\ String est une classe pas un type primitif /!\
+        String maChaine = "bidule";
+
+        int[] monTableau = {1, 5, 9, 50};
+        int[][] monAutreTableau = {{1,2}, {5,8}, {9, 50}};
+
+    }
+}
+
